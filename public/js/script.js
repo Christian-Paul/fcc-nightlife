7d88f299b7863872647e314b89afeeb128c80614
Christian Paul <christiansamuelpaul@gmail.com>
2016-09-09 12:07:52 -0400

    Changed color palette

---
+++ public/js/script.js
@@ -0,0 +1,189 @@
+var resultsList = $('.results-list');
+var searchInterface = $('.new-query-ui');
+var resultsInterface = $('.results-ui');
+var newSearch = $('.start-new-query');
+var cancelInterface = $('.cancel-interface');
+var goingButton = $('.going-button');
+var cancelButton = $('.cancel-going');
+var username;
+
+// ask server if the user has a search query
+// if user already has a query and the returned results are good, render results
+// if not, render search interface
+$('document').ready(function() {
+
+	// render search interface
+	function renderSearchInterface() {
+		searchInterface.css({ 'display': 'flex'});
+		searchInterface.hide();
+		searchInterface.show(500);
+	}
+
+
+	$.ajax({
+		url: '/api/query'
+	}).done(function(data) {
+		if(data === 'no') {
+			// no existing search query, render search interface
+			renderSearchInterface()
+		} else {
+			var data = JSON.parse(data);
+
+			if(data.docs.length < 5) {
+				// if there isn't much, or any data, render search interface
+				renderSearchInterface()
+
+			} else {
+				// if there are enough results, render bar results
+
+				// check if the user is authenticated and set username if they are
+				if(data.hasOwnProperty('userInfo') && data.userInfo['screen_name']) {
+					username = data.userInfo.screen_name;
+				}
+
+				$.each(data.docs, buildBarList);
+			}
+		}
+	}).fail(function(err) {
+		// request failed, degrade to search interface
+		renderSearchInterface()
+	});
+});
+
+
+function buildBarList(i, bar) {
+
+	var imgSrc = bar.image_url ? bar.image_url : 'https://c1.staticflickr.com/1/145/409539761_c529e76318.jpg';
+	var cancelDisplay = 'hide';
+	var goingDisplay = 'hide';
+
+	if(username) {
+		if(bar.guests.indexOf(username) !== -1) { // check if user is in guests array
+			// if user is authenticated and going to this bar, show cancel interface
+			cancelDisplay = 'show';
+		} else {
+			// if user is authenticated and not going to this bar, show going button
+			goingDisplay = 'show';
+		}
+	}
+
+	resultsList.append(
+			`<div class="bar">
+				<img src=" ${imgSrc} " alt="https://c1.staticflickr.com/1/145/409539761_c529e76318.jpg" class="bar-picture">
+				<div class="bar-interface">
+					<div class="going-ticker">${bar.guests.length} Going</div>
+					<a href="/api/add-guest/${bar.id}" class="going-button ${goingDisplay}">Join Us!</a>
+					<div class="cancel-interface ${cancelDisplay}">
+						<div class="going-notification">You're Partying Here!</div>
+						<a href="/api/remove-guest/${bar.id}" class="cancel-going">I Can't Make it</a>
+					</div>
+				</div>
+				<div class="bar-info">
+					<div class="bar-title">${bar.name}</div>
+					<div class="bar-description">${bar.snippet_text}</div>
+				</div>
+			</div>`
+		);
+
+
+
+	searchInterface.hide(500);
+
+	resultsInterface.css({'display': 'flex'});
+	resultsInterface.hide();
+	resultsInterface.show(500);
+
+	resultsList.css({'display': 'flex'});
+	resultsList.hide();
+	resultsList.show(500);
+
+}
+
+
+$('.submit-location').click(function() {
+	var userLocation = $('.user-location-input').val();
+	$('.user-location-input').val('');
+
+	if(userLocation) {
+		// only send request if the user actually input something
+
+		$.ajax({
+			// send request for bar data in this area
+			url: '/api/search/' + userLocation
+		}).done(function(data) {
+			var data = JSON.parse(data);
+			// use data to build DOM
+
+			// check if the user is authenticated and set username if they are
+			if(data.hasOwnProperty('userInfo') && data.userInfo['screen_name']) {
+				username = data.userInfo.screen_name;
+			}
+
+			$.each(data.docs, buildBarList);
+		});
+	}
+	
+});
+
+// change interface from displaying results to search interface
+$(newSearch).click(function() {
+	resultsList.hide(500);
+	resultsInterface.hide(500);
+	resultsList.empty();
+
+	searchInterface.css({'display': 'flex'});
+	searchInterface.hide();	
+	searchInterface.show(500);
+});
+
+
+// when user clicks a join button
+$('.results-list').on('click', 'a.going-button', function(e) {
+
+	// prevent default request from being sent
+	e.preventDefault();
+	var ele = $(this);
+	var link = ele[0].href;
+
+	$.ajax({
+		url: ele[0].href
+	}).done(function(doc) {
+
+		// update number of people going
+		ele.siblings('.going-ticker').text(doc.guests.length);
+
+		// toggle interface
+		ele.toggle(500);
+		ele.siblings('.cancel-interface').toggle(500);
+
+	})
+
+	return false;
+
+})
+
+// when user clicks a cancel button
+$('.results-list').on('click', 'a.cancel-going', function(e) {
+
+	// prevent default request from being sent
+	e.preventDefault();
+	var ele = $(this);
+	var link = ele[0].href;
+
+	$.ajax({
+		url: ele[0].href
+	}).done(function(doc) {
+		console.log(doc.guests.length);
+
+		// update number of people going
+
+		ele.parent().siblings('.going-ticker').text(doc.guests.length);
+
+		// toggle interface
+		ele.parent().toggle(500);
+		ele.parent().siblings('.going-button').toggle(500);
+	})
+
+	return false;
+
+})
--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var mongoose = require('mongoose');
 var Twitter = require('node-twitter-api');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var async = require('async');
 require('express-helpers')(app);
 app.enable('trust proxy');
 var port = process.env.PORT || 3000;
+var yelp = require("node-yelp");
 
 // get credentials from config file in dev, or from heroku env in deployment
 if(port === 3000) {
@@ -18,6 +20,10 @@ if(port === 3000) {
 		mongoosePassword: process.env.mongoosePassword,
 		twitterConsumerKey: process.env.twitterConsumerKey,
 		twitterConsumerSecret: process.env.twitterConsumerSecret,
+		yelpConsumerKey: process.env.yelpConsumerKey,
+		yelpConsumerSecret: process.env.yelpConsumerSecret,
+		yelpToken: process.env.yelpToken,
+		yelpTokenSecret: process.env.yelpTokenSecret,
 		callbackUrl: process.env.callbackUrl
 	};
 }
@@ -96,11 +102,135 @@ app.get('/sign-out', function(req, res) {
 	})
 });
 
+// yelp setup
+var client = yelp.createClient({
+	oauth: {
+		"consumer_key": config.yelpConsumerKey,
+		"consumer_secret": config.yelpConsumerSecret,
+		"token": config.yelpToken,
+		"token_secret": config.yelpTokenSecret
+	}
+});
+
+// database setup
+mongoose.connect('mongodb://' + config.mongooseUsername + ':' + config.mongoosePassword + '@ds017636.mlab.com:17636/party-party');
+
+var barSchema = new mongoose.Schema({
+	name: String,
+	snippet_text: String,
+	id: String,
+	image_url: String,
+	queries: [String],
+	guests: [String],
+	attendees: {type: Number, default: 0}
+});
+
+var Bar = mongoose.model('Bar', barSchema)
+
 // begin app
 app.listen(port, function(req, res) {
 	console.log('listening on 3000');
 });
 
 app.get('/', function(req, res) {
-	res.render('index.ejs', {bars: 'bars', userInfo: req.session.userInfo});
+	res.render('index.ejs', {userInfo: req.session.userInfo});
+});
+
+app.get('/api/search/:tagId', function(req, res) {
+	var userQuery = req.params.tagId;
+	req.session.query = userQuery;
+
+	client.search({
+		terms: "bar",
+		location: userQuery,
+		limit: 10,
+		category_filter: 'bars'
+	}).then(function (data) {
+		var results = data.businesses;
+
+
+		// asynchronously save results to database
+		// wait for this to finish before sending client response
+		// so user doesn't break things
+		async.each(results, function(result, callback) {
+
+			var query = { id: result.id };
+			// only set 'setOnInsert' properties when doing an upsert
+			// addToSet will create a queries field with userquery as its element on upsert
+			// on update, addToSet will push the value to the array only if the array doesn't already have it
+			var update = {
+				$setOnInsert: {
+					name: result.name,
+					image_url: result.image_url,
+					snippet_text: result.snippet_text
+				},
+				$addToSet: { queries: userQuery }
+			};
+			var options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+			Bar.findOneAndUpdate(query, update, options, function(err, doc) {
+				if(err) {
+					// if this is called with an error, the async process stops
+					callback(err);
+				} else {
+					// when all of these are called, the next function is processed
+					callback();
+				}
+			});
+
+		}, function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				// all docs saved
+				// add guests array to each results because client expects all data to have that schema
+				for(var i = 0; i < results.length; i++) {
+					results[i].guests = [];
+				}
+				res.json(JSON.stringify({ docs: results, userInfo: req.session.userInfo }));
+			}
+		});
+	}).catch(function (err) {
+		console.log(err);
+	});
+});
+
+app.get('/api/query', function(req, res) {
+
+	if(req.session.hasOwnProperty('query') && req.session.query) {
+		Bar.find({ queries: req.session.query }, function(err, docs) {
+			res.json(JSON.stringify({ docs: docs, userInfo: req.session.userInfo }));
+		});
+	} else {
+		res.send('no');
+	}
+
+});
+
+app.get('/api/remove-guest/:tagId', function(req, res) {
+	var barId = req.params.tagId;
+	var guest = req.session.userInfo.screen_name;
+	console.log(barId, guest);
+
+	Bar.findOneAndUpdate({ id: barId }, { $pull: { guests: guest } }, { new: true }, function(err, doc) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(doc);
+			res.send(doc);
+		}
+	})
+});
+
+app.get('/api/add-guest/:tagId', function(req, res) {
+	var barId = req.params.tagId;
+	var guest = req.session.userInfo.screen_name;
+
+	Bar.findOneAndUpdate({ id: barId }, { $addToSet: { guests: guest } }, { new: true }, function(err, doc) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.send(doc);
+		}
+	})
 });
